89e50100d585dadfd80c2cd442b4ebdd52215e98
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-16 22:02:30 -0600

    Add rails version to Gemfile Including Capybara within rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Capybara::DSL
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
