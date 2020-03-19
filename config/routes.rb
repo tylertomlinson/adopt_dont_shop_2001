809af153471125b708635ef542bce5cf5570345a
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-19 17:14:42 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
+  post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
-  post '/shelters/:id', to: 'shelters#show'
+  get '/shelters/:id/edit', to: 'shelters#edit'
+  patch '/shelters/:id', to: 'shelters#update'
+  delete '/shelters/:id', to: 'shelters#destroy'
+
+
+   # resources :shelters
 end
