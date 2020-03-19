97236991832a3dcbb7ae0822334f55cc4e523548
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-18 20:28:17 -0600

    Format on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,24 @@
-puts "seeding the dev database"
-
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
 Shelter.destroy_all
+ 
+Shelter.create(       name:     "Pet Palace",
+                      address:  "12483 Jefferson Drive",
+                      city:     "Denver",
+                      state:    "CO",
+                      zip:      "80205")
 
-Shelter.create(name: "Pet Palace", address:  "12483 Jefferson Drive", city:     "Denver", state:    "CO", zip:      "80205")
 
-
-Shelter.create(name: "ZIFGGY FAIRN", address:  "irnrinrinrin", city:     "Denver", state:    "CO", zip:      "80205")
-
+Shelter.create(       name:     "Save a Pet",
+                      address:  "38372 Main",
+                      city:     "Fort Collins",
+                      state:    "CO",
+                      zip:      "80205")
 
 
 puts "Created #{Shelter.count} Shelters"
