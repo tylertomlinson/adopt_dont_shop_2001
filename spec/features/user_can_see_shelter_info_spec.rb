ded42bfcf47e0cd090daa8bf4b49ad6692b1541e
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-19 17:14:34 -0600

    Update on test

--- spec/features/user_can_see_shelter_info_spec.rb
+++ spec/features/user_can_see_shelter_info_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "shelter show page", type: :feature do
   it "can see shelter with name address city state zip" do
@@ -8,14 +8,28 @@ RSpec.describe "shelter show page", type: :feature do
                                state:    "CO",
                                zip:      "80205")
 
+    shelter_2 = Shelter.create(name:    "Pet Paradise",
+                               address: "4876 Mountain View",
+                               city:    "Denver",
+                               state:   "CO",
+                               zip:     "80232")
+
 
     visit "/shelters/#{shelter_1.id}"
 
     expect(page).to have_content(shelter_1.name)
-    expect(page).to have_content("#{shelter_1.name}")
     expect(page).to have_content("Address: #{shelter_1.address}")
     expect(page).to have_content("City: #{shelter_1.city}")
     expect(page).to have_content("State: #{shelter_1.state}")
     expect(page).to have_content("Zip: #{shelter_1.zip}")
+
+
+    visit "/shelters/#{shelter_2.id}"
+
+    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content("Address: #{shelter_2.address}")
+    expect(page).to have_content("City: #{shelter_2.city}")
+    expect(page).to have_content("State: #{shelter_2.state}")
+    expect(page).to have_content("Zip: #{shelter_2.zip}")
   end
 end
