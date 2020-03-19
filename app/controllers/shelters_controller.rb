a4f9548bba8ae8c91067547d0bdb026fd7b27c42
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-19 17:15:09 -0600

    Add methods to perform crud

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -4,7 +4,48 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
-  def show
-    @shelter = Shelter.find(params[:id])
+  def new
   end
+
+  def create
+     shelter = Shelter.new({
+       name: params[:shelter][:name],
+       address: params[:shelter][:address],
+       city: params[:shelter][:city],
+       state: params[:shelter][:state],
+       zip: params[:shelter][:zip]})
+
+     shelter.save
+
+     redirect_to "/shelters"
+  end
+
+ def edit
+   @shelter = Shelter.find(params[:id])
+ end
+
+ def update
+   shelter = Shelter.find(params[:id])
+   shelter.update({
+                   name:     params[:shelter][:name],
+                   address:  params[:shelter][:address],
+                   city:     params[:shelter][:city],
+                   state:    params[:shelter][:state],
+                   zip:      params[:shelter][:zip]
+                  })
+   shelter.save
+
+   redirect_to "/shelters/#{shelter.id}"
+ end
+
+ def destroy
+   Shelter.destroy(params[:id])
+
+   redirect_to "/shelters"
+ end
+
+ def show
+   @shelter = Shelter.find(params[:id])
+ end
+
 end
