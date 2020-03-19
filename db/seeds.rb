5ab9fdf8f9feae2b3f71dfed83a01d62a32cd4ba
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-19 17:15:41 -0600

    Add view for edit and new Updated show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Shelter.destroy_all
- 
+
 Shelter.create(       name:     "Pet Palace",
                       address:  "12483 Jefferson Drive",
                       city:     "Denver",
