575629149fdb7f2f2406c86f1b9aed0a81374081
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-19 22:26:31 -0600

    Add pets route index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'pets/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
@@ -11,6 +9,9 @@ Rails.application.routes.draw do
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
 
+  get '/pets', to: 'pets#index'
+
 
    # resources :shelters
+   # resources :pets
 end
