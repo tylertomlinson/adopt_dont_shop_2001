8ce3258fb057d85c8b0f73af3ee09d7ba1652cef
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-21 19:53:00 -0600

    Pets show page spec test

---
+++ spec/features/pets/user_can_see_each_pet_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "pets index page", type: :feature do
+  it "can see each pet with image name age sex shelter" do
+    shelter_1 = Shelter.create(name:     "Pet Palace",
+                               address:  "12483 Jefferson Drive",
+                               city:     "Denver",
+                               state:    "CO",
+                               zip:      "80205")
+
+    shelter_2 = Shelter.create(name:    "Pet Paradise",
+                               address: "4876 Mountain View",
+                               city:    "Denver",
+                               state:   "CO",
+                               zip:     "80232")
+
+    pet_1 = Pet.create(image: "https://i.imgur.com/wKls5bM.png",
+                       name:  "Ziggy",
+                       age: "1",
+                       sex: "male",
+                       shelter_id: (shelter_1.id))
+
+    pet_2 = Pet.create(image: "https://i.imgur.com/wKls5bM.png",
+                       name:  "Ziggy2",
+                       age: "2",
+                       sex: "male",
+                       shelter_id: (shelter_2.id))
+
+    visit '/pets'
+
+    expect(page.find("#img-#{pet_1.id}")['src']).to have_content "#{pet_1.image}"
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_1.age)
+    expect(page).to have_content(pet_1.sex)
+    expect(page).to have_content(pet_1.shelter.name)
+
+    expect(page.find("#img-#{pet_2.id}")['src']).to have_content "#{pet_2.image}"
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_2.age)
+    expect(page).to have_content(pet_2.sex)
+    expect(page).to have_content(pet_2.shelter.name)
+  end
+end
