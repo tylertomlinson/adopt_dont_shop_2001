77b16ec40e4a98c25c7ac032a982ace0e91488dc
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-21 19:14:06 -0600

    Add create to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,17 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def create
+     pet = Pet.new ({
+      image: params[:image],
+      name: params[:name],
+      approximate_age: params[:approximate_age],
+      sex: params[:sex],
+      shelter: params[:shelter]})
+
+    pet.save
+
+    redirect_to "/pets"
+  end
 end
