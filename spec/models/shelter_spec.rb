50edea33950740de0e03c1f590a7b0a0903778ed
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-23 12:03:19 -0600

    Remove Instance variables on model testing

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,35 +1,7 @@
 require 'rails_helper'
 
 describe Shelter, type: :model do
-  before(:each) do
-
-    @shelter_1 = Shelter.create(name:     "Pet Palace",
-                                address:  "12483 Jefferson Drive",
-                                city:     "Denver",
-                                state:    "CO",
-                                zip:      "80205")
-
-    @pet_1 = Pet.create(image: "https://i.imgur.com/wKls5bM.png",
-                        name:  "Ziggy",
-                        age: "1",
-                        sex: "male",
-                        shelter_id: (@shelter_1.id))
-
-    @pet_2 = Pet.create(image: "https://i.imgur.com/wKls5bM.png",
-                        name:  "Ziggy2",
-                        age: "2",
-                        sex: "male",
-                        shelter_id: (@shelter_1.id))
-  end
-
-  describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :address}
-    it {should validate_presence_of :city}
-    it {should validate_presence_of :state}
-    it {should validate_presence_of :zip}
-  end
-
+  
   describe "relationships" do
     it {should have_many :pets}
   end
