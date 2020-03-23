6d6a3b0b9124b695e11a3cd53caa1b8f6003367d
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-23 01:34:44 -0600

    extension story

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,9 @@ class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
 
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def pet_count
+   pets.count
+  end
+
 end
